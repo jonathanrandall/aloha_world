e48b3978dff397915e80cb8ad3e5385465a25ca1
jonathanrandall <jonnyrandall@live.com.au>
2024-03-23 13:53:22 +1100

    new mobile pubsub

---
+++ contol_ws/src/mobile_pubsub/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'mobile_pubsub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jonny',
+    maintainer_email='jonny@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = mobile_pubsub.publisher_member_function:main',
+        ],
+    },
+)
